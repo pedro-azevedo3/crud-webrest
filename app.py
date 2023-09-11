8f287162d6d2c403218050607d7bfc06996b41b3
Pedro <pedroazevedoteixeiracontato@gmail.com>
2023-09-11 20:48:28 -0300

    Updated gitignore

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import flask
+import healthcheck from app.api.routers
 
 app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET'])
-def healthcheck():
-    return 'WORKING'
-
+healthcheck()
 
 if __name__ == "__main__":
     app.run(port=8080, debug=False)
